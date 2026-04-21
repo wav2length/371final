43bc74e91b50dd0a49332d642b992baec73c5539
cwelstead <welstead.cole@gmail.com>
2026-04-21 10:13:14 -0500

    Brought back app.jsx for better prop handling

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -1,35 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home.jsx'
-import Onboarding1 from './OnBoarding/Onboarding1.jsx'
-import Onboarding2 from './OnBoarding/Onboarding2.jsx'
-import Onboarding3 from './OnBoarding/Onboarding3.jsx'
-import OnboardingComplete from './OnBoarding/OnboardingComplete.jsx'
-import Career from './Career.jsx'
-import Survey from './Surveys/Survey.jsx'
-import MatchesPage from './MatchesPage.jsx'
-import Chat from './Chat.jsx'
-import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Onboarding pages */}
-        <Route path="/onboarding1" element={<Onboarding1 />} />
-        <Route path="/onboarding2" element={<Onboarding2 />} />
-        <Route path="/onboarding3" element={<Onboarding3 />} />
-        {/* survey rating questions */}
-        <Route path="/survey" element={<Survey />} />
-        {/* Career page  */}
-        <Route path="/career" element={<Career />} />
-        {/* End onboarding pages  */}
-        <Route path="/onboarding-complete" element={<OnboardingComplete />} />
-        {/* Matches and Chatting Pages */}
-        <Route path="/matches" element={<MatchesPage />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
